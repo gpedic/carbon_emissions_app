ec6ae1f9bcffda094e6a5812ec90a5844bed62d1
Gary-Yau Chan <gary-yauchan@Gary-Yaus-Air.home>
2016-02-21 11:50:34 -0500

    added bg and fixed transition

---
+++ transitions.js
@@ -0,0 +1,61 @@
+function reset(){
+
+    // TOGGLE PANELS
+    close_to_default();   
+}
+
+function close_to_default(){
+    // SHOW NAV BAR
+    $('#main-nav').animate({bottom: '+=500px'}, 500);
+
+    // CLOSE UP DATA PANEL
+    $('.description').animate({bottom: '-=450px'}, 750);
+
+    // HIDE CLOSE BUTTON
+    $('#close-button').hide();
+
+    // SHOW COVER
+    $('.image-cover').show();
+}
+
+function default_to_open(){
+    // HIDE NAV BAR
+    $('#main-nav').animate({bottom: '-=500px'}, 500);
+
+    // SHOW CLOSE BUTTON
+    $('#close-button').show();
+
+    // OPEN UP DATA PANEL
+    $('.description').animate({bottom: '+=450px'}, 750);
+
+    // HIDE COVER
+    $('.image-cover').hide();
+}
+
+function default_to_library(){
+    // HIDE NAV BAR
+    $('#main-nav').animate({bottom: '-=500px'}, 500);
+
+    // SHOW LIBRARY
+    $('#library').show();
+
+    // SHOW CLOSE BUTTON
+    $('#library-close-button').show();
+
+    // HIDE COVER
+    $('.image-cover').hide();
+}
+
+function library_to_default(){
+    // SHOW NAV BAR
+    $('#main-nav').animate({bottom: '+=500px'}, 500);
+
+    // HIDE LIBRARY
+    $('#library').hide(); 
+
+    // HIDE CLOSE BUTTON
+    $('#library-close-button').hide();
+
+    // SHOW COVER
+    $('.image-cover').show();
+}
